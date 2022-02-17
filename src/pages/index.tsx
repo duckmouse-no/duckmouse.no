494d26024a0ff42f0b7762aa76fceb64fc233aa7
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-02-17 19:23:23 +0100

    Add navbar (#5)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Navbar } from '../components/Navbar';
 
 const IndexPage = () => (
   <main>
     <title>duckmouse.no</title>
+    <Navbar />
     <h1 className='text-3xl font-bold underline'>duckmouse.no</h1>
   </main>
 );
