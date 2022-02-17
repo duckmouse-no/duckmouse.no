c6e91b2bdd16e57c5b5268e5844f665f168771ab
isaksolheim <isak.m.h.s@gmail.com>
2022-02-17 18:38:49 +0100

    Add tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,21 @@
 module.exports = {
   siteMetadata: {
-      title: `duckmouse.no`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `duckmouse.no`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
     },
-    __key: "images"
-  }]
+  ],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const IndexPage = () => (
   <main>
     <title>duckmouse.no</title>
-    <h1>duckmouse.no</h1>
+    <h1 className='text-3xl font-bold underline'>duckmouse.no</h1>
   </main>
 );
 
