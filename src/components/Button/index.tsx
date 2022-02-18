7137d4d7b3029fb11c1e3bbb4542fcb4a20f09b1
isaksolheim <isak.m.h.s@gmail.com>
2022-02-18 16:00:16 +0100

    Add inactive button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,8 +3,17 @@ import './button.css';
 
 interface ButtonProps {
   label: string;
+  type?: 'primary' | 'inactive';
 }
 
-export const Button = ({ label }) => (
-  <button className='bg-sky-300 pt-4 pb-4 pl-8 pr-8'>{label}</button>
-);
+export const Button = ({ label, type = 'primary' }) => {
+  const getClassName = () => {
+    return type === 'primary'
+      ? 'button--primary bg-sky-300 pt-4 pb-4 pl-8 pr-8'
+      : 'button--inactive bg-gray-300 pt-4 pb-4 pl-8 pr-8 cursor-default';
+  };
+
+  const className = getClassName();
+
+  return <button className={className}>{label}</button>;
+};
