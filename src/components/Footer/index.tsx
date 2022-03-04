ddedfee2cd285a71426da9d8ce7e3de4c05e6dbd
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-03-04 13:22:41 +0100

    Update invalid email error message (#16)
    * Update invalid email error message
    * Fix footer bug on small screens

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () => (
   <footer className='w-screen text-center'>
-    <p className='absolute inset-x-0 bottom-4 text-xs text-slate-500 flex-grow mt-8'>
+    <p className='tall:absolute tall:inset-x-0 tall:bottom-0 text-xs text-slate-500 flex-grow mt-8 mb-8 tall:mb-4'>
       Ⓒ duckmouse.no / 2022
     </p>
   </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      screens: {
+        tall: { raw: '(min-height: 600px)' },
+      },
       colors: {
         btnA: '#B3D1F0',
         btnI: '#C2C7CB',
