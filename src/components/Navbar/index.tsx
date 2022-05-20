c3f1f467b9ed842a140af539510c6c04c137684d
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-05-20 14:13:56 +0200

    Fix navbar padding (#18)

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Navbar = () => (
-  <nav className='w-screen border-b-4 p-4'>
+  <nav className='w-screen border-b-4 pt-4 pb-3'>
     <p className='text-center text-2xl'>duckmouse.no</p>
   </nav>
 );
