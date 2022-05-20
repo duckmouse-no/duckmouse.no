79a620569bb5c2e6777acf1364d7734964de8186
isaksolheim <isak.m.h.s@gmail.com>
2022-05-20 13:56:08 +0200

    Remove logo from navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import background from '../../images/bg.png';
 
 export const Navbar = () => (
-  <nav
-    className='w-screen border-b-4 p-6 bg-repeat'
-    style={{ backgroundImage: `url(${background})` }}
-  />
+  <nav className='w-screen border-b-4 p-4'>
+    <p className='text-center text-2xl'>duckmouse.no</p>
+  </nav>
 );
