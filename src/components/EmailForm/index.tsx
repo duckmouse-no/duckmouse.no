3ff8c76beafaf9a750fbb4fbb731e4880f99deae
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-08-26 14:32:44 +0200

    Remove images from landing page (#21)

--- src/components/EmailForm/index.tsx
+++ src/components/EmailForm/index.tsx
@@ -1,45 +1,47 @@
-import React, { useState } from 'react';
-import { validateEmail } from '../../utils/email';
-import { addEmail } from '../../utils/firestore';
-import { Button } from '../Button';
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/email";
+import { addEmail } from "../../utils/firestore";
+import { Button } from "../Button";
 
 export const EmailForm = () => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [email, setEmail] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = () => {
     if (validateEmail(email)) {
       addEmail(email);
       setShowSuccessMessage(true);
     } else {
-      setError('Ugyldig epost adresse');
+      setError("Ugyldig epost adresse");
       setShowSuccessMessage(false);
     }
-    setEmail('');
+    setEmail("");
   };
 
   const handleInput = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    if (error !== '') setError('');
+    if (error !== "") setError("");
     setEmail(evt.target.value);
   };
 
   return (
-    <div className='mt-8 px-8'>
-      <p className='text-gray-800 mb-2'>Meld deg på for å få oppdateringer:</p>
-      <div className='flex justify-center flex-col sm:flex-row'>
+    <div className="mt-8 px-8">
+      <p className="text-gray-800 mb-2 m-auto sm:max-w-[338px] text-left">
+        Meld deg på for å bli varlset når butikken åpner:
+      </p>
+      <div className="flex justify-center flex-col sm:flex-row">
         <input
-          className='email-form mr-4 drop-shadow-btnA text-gray-600 p-2 w-full mb-2 sm:mb-0 sm:w-56'
-          type='text'
-          placeholder='Skriv inn epost_'
+          className="email-form mr-4 drop-shadow-btnA text-gray-600 p-2 w-full mb-2 sm:mb-0 sm:w-56"
+          type="text"
+          placeholder="Skriv inn epost_"
           value={email}
           onChange={handleInput}
         />
-        <Button size='small' label='Send inn' onClick={handleSubmit} />
+        <Button size="small" label="Send inn" onClick={handleSubmit} />
       </div>
-      <span className='block mt-4 text-red-800'>{error}</span>
+      <span className="block mt-4 text-red-800">{error}</span>
       {showSuccessMessage && (
-        <span className='block mt-4'>Takk, vi holder deg oppdatert!</span>
+        <span className="block mt-4">Takk, vi holder deg oppdatert!</span>
       )}
     </div>
   );
