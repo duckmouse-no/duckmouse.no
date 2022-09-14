7bd829eedab19c0f82d87a60969533ae3596887a
isaksolheim <mail@isaks.io>
2022-09-14 10:27:34 +0200

    Show Buy With Vipps button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Layout } from "../templates/Layout";
 import { EmailForm } from "../components/EmailForm";
+import { VippsButton } from "../components/VippsButton";
 
 const IndexPage = () => (
   <Layout>
     <main className="mt-16">
       <EmailForm />
+      <VippsButton />
     </main>
   </Layout>
 );
