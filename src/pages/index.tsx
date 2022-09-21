2e45cf0db78a0a46d768dc3eea6cb787af447d09
isaksolheim <mail@isaks.io>
2022-09-21 22:13:34 +0200

    Refactor carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Layout } from "../templates/Layout";
-import { EmailForm } from "../components/EmailForm";
-import { Navbar } from "../components/Navbar";
-import { Mouse } from "../components/Mouse";
-import { Desc } from "../components/Desc";
-import { Button } from "../components/Button";
-import { StaticImage } from "gatsby-plugin-image";
-import { MouseTitle } from "../components/MouseTitle";
 import { Carousel } from "../components/Carousel";
 import { Wire } from "../components/Wire";
 import { About } from "../components/About";
 
 const IndexPage = () => {
-  const [visible, setVis] = useState(true);
+  const [showWire, setShowWire] = useState(false);
 
   return (
     <Layout>
       <main>
         <div className="md:w-750 m-auto grid grid-cols-1 md:grid-cols-2 mb-40">
-          <Wire show={visible} />
-          <p></p>
-          <Carousel callback={setVis} />
+          <Wire show={showWire} />
+          <p />
+          <Carousel setShowWire={setShowWire} />
           <About />
         </div>
       </main>
