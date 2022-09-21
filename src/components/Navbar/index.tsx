ff8a08b5b1665413fe5a68bdb684ea3cb589f451
isaksolheim <mail@isaks.io>
2022-09-21 18:00:52 +0200

    Add selected page highlighting

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,49 @@
 import { Link } from "gatsby";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export const Navbar = () => (
-  <nav className="w-screen py-4 border-b-2 flex place-content-evenly">
-    <ul className="flex flex-row gap-8">
-      <li className="text-gray-900">
-        <Link to="/">duckmouse</Link>
-      </li>
-      <li className="text-gray-600">
-        <Link to="/om-oss">om oss</Link>
-      </li>
-      <li className="text-gray-600">
-        <Link to="/faq">faq</Link>
-      </li>
-    </ul>
-  </nav>
-);
+export const Navbar = () => {
+  const [selectedTab, setSelectedTab] = useState("/");
+
+  useState(() => {
+    const pathname = window.location.pathname;
+    setSelectedTab(pathname);
+  }, []);
+
+  return (
+    <nav className="w-screen py-4 border-b-2 flex place-content-evenly">
+      <ul className="flex flex-row gap-8">
+        <li
+          className={
+            selectedTab === "/" ? "text-black font-medium" : "text-gray-600"
+          }
+        >
+          <Link to="/" onClick={() => setSelectedTab("home")}>
+            duckmouse
+          </Link>
+        </li>
+        <li
+          className={
+            selectedTab === "/om-oss"
+              ? "text-gray-900 font-medium"
+              : "text-gray-600"
+          }
+        >
+          <Link to="/om-oss" onClick={() => setSelectedTab("om-oss")}>
+            om oss
+          </Link>
+        </li>
+        <li
+          className={
+            selectedTab === "/faq"
+              ? "text-gray-900 font-medium"
+              : "text-gray-600"
+          }
+        >
+          <Link to="/faq" onClick={() => setSelectedTab("faq")}>
+            faq
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
