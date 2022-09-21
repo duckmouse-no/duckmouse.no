13f6fd8a72db5d3aae2f8a7258c14424211a8cc6
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-09-21 18:47:28 +0200

    Fix navbar build bug (#31)

--- src/components/MouseTitle/index.tsx
+++ src/components/MouseTitle/index.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
 
 export const MouseTitle = () => (
   <div className="flex flex-col  text-left w-full mb-2">
-    <p className="text-4xl lg:text-6xl">Duckmouse</p>
-    <p className="text-m">Den siste musen du noengang vil trenge.</p>
-    <p>300,-</p>
+    <p className="text-4xl lg:text-4xl font-bold text-gray-800">duckmouse</p>
+    <p className="font-bold">300,-</p>
+    <p>Quack quack! 🐤</p>
   </div>
 );
