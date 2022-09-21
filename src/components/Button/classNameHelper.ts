298f21cca61277a702c8d11f8d39c96e20ac6b16
Isak Solheim <42111513+isaksolheim@users.noreply.github.com>
2022-09-21 15:12:01 +0200

    Implement fat footer (#25)

--- src/components/Button/classNameHelper.ts
+++ src/components/Button/classNameHelper.ts
@@ -1,28 +1,28 @@
 export const getClassName = (
-  size: 's' | 'm' | 'l' | 'xl',
-  type: 'squere' | 'round',
+  size: "s" | "m" | "l" | "xl",
+  type: "squere" | "round",
   active: true | false
 ) => {
-  let className = '';
+  let className = "";
 
-  if (size === 's') {
-    className += 'py-2 px-8';
-  } else if (size === 'm') {
-    className += 'text-lg py-4 px-10';
-  } else if (size === 'l') {
-    className += 'text-lg py-4 px-14';
-  }  else if (size === 'xl') {
-    className += 'text-2xl py-5 px-18';
+  if (size === "s") {
+    className += "py-2 px-8";
+  } else if (size === "m") {
+    className += "text-lg py-4 px-10";
+  } else if (size === "l") {
+    className += "text-lg py-4 px-14";
+  } else if (size === "xl") {
+    className += "text-2xl py-5 px-18";
   }
 
-  if (type === 'round') {
-    className += ' rounded-full';
+  if (type === "round") {
+    className += " rounded-full";
   }
 
   if (active) {
-    className += ' bg-btnA drop-shadow-btnA';
+    className += " bg-btnA drop-shadow-btnA";
   } else if (!active) {
-    className += ' bg-btnI drop-shadow-btnI cursor-default';
+    className += " bg-btnI drop-shadow-btnI cursor-default";
   }
 
   return className;
