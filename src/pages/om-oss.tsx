af3c3fd528f58ced724e79c8a98bd3e7d10ebf77
Gerhard Gustasven <69513686+Sensay1@users.noreply.github.com>
2022-10-03 20:42:28 +0200

    New text (#41)
    Write new text

--- src/pages/om-oss.tsx
+++ src/pages/om-oss.tsx
@@ -8,11 +8,40 @@ const AboutUsPage = () => (
     <main className="px-8 md:w-750 md:m-auto">
       <h1 className="text-2xl font-bold mt-8">Om Oss</h1>
       <p className="my-4">
-        Isak og Gerhard har hatt kjempelyst på en veldig spesiell datamus. En
-        datamus med veldig søt søt, hvit og blå og som har en liten gummiand
-        inni seg. For å få tak i denne musa måtte vi produsere minimum 500stk.
-        Derfor har Isak og Gerhard nå startet en bedrift. Hva ellers kunne vi ha
-        gjort?
+        For rundt 3 år siden skjedde det noe kjempestort. Isak Solheim fant den
+        kuleste pc-musen noen noensinne hadde funnet. Nemlig duckmouse. På en
+        obskur nettside langt ned i darkweb, så langt at du blir nødt til å
+        trykke på google sin andre fane var det noen som solgte en pc mus. Denne
+        pc musen hadde et kammer og i kammeret var det en badeand. Det var da
+        Isak innså fremtiden. Han skjønte med en gang at dette var musen som kom
+        til å revolusjonere alt.
+      </p>
+      <p className="my-4">
+        Det var kun et problem. Musen var utsolgt for flere år siden. Med
+        reverse image search og andre hackermetoder fant Isak andre som også
+        hadde solgt musen, men også disse var utsolgt. Etter mange måneder med
+        søking og researching var ble det konkludert at duckmouse var fortiden
+        til.
+      </p>
+      <p className="my-4">
+        Likevel gir ikke Isak opp. I en drøm får han en brilliant ide. Hva om
+        han bare startet en bedrift og startet å produsere pc musene selv.
+        Likevel tokk det overaskende lang tid før han møtte noen som så
+        genialiteten i ideen.
+      </p>
+      <p className="my-4">
+        Nå 3 år seinere etter å ha snakket med de gamle produsentene, laget en
+        nettbutikk, fikset innpakning og brukt tullete mye penger har Gerhard og
+        Isak begynt å selge det legendariske siste partiet med duckmouse! Vi er
+        nå avhengige av at DERE gjør deres del og kjøper en før vi går tomme.
+        Dette er jo DUGNAD!
+      </p>
+      <p className="my-4">
+        Det er både trist og rørende å tenke på at om flere år en dag langt inn
+        i fremtiden kommer noen andre til å finne siden vår med de utsolgte
+        datamusene og bestemme seg for å produsere et pari til med mus. Slik tar
+        vi del i en lang tradisjon med datamus selgere som sikkert strekker seg
+        langt tilbake i tid.
       </p>
       <div className="sm:flex sm:flex-row gap-4">
         <FadeInSection>
