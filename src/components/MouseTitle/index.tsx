7ba9020b00c25dbd863759c8fea8c3f16935b70e
isaksolheim <mail@isaks.io>
2022-10-04 11:43:19 +0200

    Update price

--- src/components/MouseTitle/index.tsx
+++ src/components/MouseTitle/index.tsx
@@ -4,6 +4,6 @@ export const MouseTitle = () => (
   <div className="flex flex-col w-full mb-2">
     <p className="text-6xl">Duckmouse</p>
     <p className="text-m">Den siste musen du noengang vil trenge.</p>
-    <p>300,-</p>
+    <p>199,-</p>
   </div>
 );
